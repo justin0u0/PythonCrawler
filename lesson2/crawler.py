30a3b5002a399d31d5d62124c43509def80e2cde
justin <dancinglinkxalgorithm@gmail.com>
2019-07-09 00:30:08 +0800

    simplify lesson 2

--- lesson2/crawler.py
+++ lesson2/crawler.py
@@ -3,11 +3,10 @@ from bs4 import BeautifulSoup
 import lxml
 from download import downloader
 
-url = "https://www.dcard.tw"
-res = requests.get(url + "/f/pet")
+res = requests.get("https://www.dcard.tw/f/pet")
 soup = BeautifulSoup(res.text, "lxml")
 for a in soup.find_all("a", class_="PostEntry_root_V6g0rd"):
-    res = requests.get(url + a["href"])
+    res = requests.get("https://www.dcard.tw" + a["href"])
     soup = BeautifulSoup(res.text, "lxml")
     tags = soup.find_all("a", class_="TopicList_topic_1XGOjs")
     if "貓" in [tag.text for tag in tags]:
